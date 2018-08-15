42b7e226e06048da52294b3b2a964de87c2e698b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:37:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(array)
   if array.any? == false 
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    puts "The line is currently: #{array."
   end
 end 
