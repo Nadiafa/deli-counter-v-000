7d38bb50491e857c7b3b2a0f9366b10fd4cb39c8
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-15 16:44:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,4 +7,46 @@ def line(array)
     array.each { |p, i| person=p, position=i+1}
     puts "The line is currently: #{position"
   end
+  
+  
+  if (line.length === 0) {
+    return `The line is currently empty.`
+  } 
+  var katzDeliLine = []
+  for (var i=0, l=line.length; i<l; i++){
+    katzDeliLine.push(`${i + 1}. ${line[i]}`)
+  } 
+  return `The line is currently: ${katzDeliLine.join(', ')}` 
+  
+  
+  
+  
+  
+  
 end 
+
+=begin
+function takeANumber(line, name) {
+line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return `There is nobody waiting to be served!`
+  } 
+  return `Currently serving ${line.shift()}.`
+  }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return `The line is currently empty.`
+  } 
+  var katzDeliLine = []
+  for (var i=0, l=line.length; i<l; i++){
+    katzDeliLine.push(`${i + 1}. ${line[i]}`)
+  } 
+  return `The line is currently: ${katzDeliLine.join(', ')}` 
+      
+}
+=end
